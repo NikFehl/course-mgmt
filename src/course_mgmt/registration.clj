df8a1282965ae4707272097db2f3236d8e81a62b
NikFehl <niklasfehl@t-online.de>
2019-12-28 15:33:04 +0100

    added CSS milligram

--- src/course_mgmt/registration.clj
+++ src/course_mgmt/registration.clj
@@ -15,17 +15,23 @@
 (defn registration
   [request]
     (html5  {:lang "en"}
+      [:head
+        (include-css "css/milligram.css")]
       [:body
-        [:div [:h1 "Hier kommen die Daten rein:" ]]
-        [:div {:data-offset-top "250"}
-                    (form-to [:post "./register"]
-                             [:fieldset
-                            [:p   [:label "firstname"]
-                              (text-field :firstname )]
-                            [:p   [:label "lastname"]
-                              (text-field :lastname )]
-                            [:p   [:label "Comment"]
-                              (text-area {:rows 5} :comment )]
-                              [:button.btn {:type "submit"} "Submit!"]
-                              (anti-forgery-field)])]]
+        [:div.container [:h1 "Registration" ]]
+        [:div.container {:data-offset-top "250"}
+            (form-to [:post "./register"]
+              [:fieldset
+                [:div.row
+                    [:div.column.column-25 [:label "Vorname:"]]
+                    [:div.column.column-25 (text-field :firstname )]]
+                [:div.row
+                    [:div.column.column-25 [:label "Nachname"]]
+                    [:div.column.column-25 (text-field :lastname )]]
+                [:div.row
+                    [:div.column.column-25 [:label "Kommentar"]]
+                    [:div.column.column-25 (text-area {:rows 5} :comment )]]
+                [:div.row
+                    [:div.column.column-25 [:button {:type "submit"} "registrieren"]]]
+                    (anti-forgery-field)])]]
       [:headers {}]))
--- src/course_mgmt/userlisting.clj
+++ src/course_mgmt/userlisting.clj
@@ -16,22 +16,26 @@
 (defn userlisting
   [users]
   (html5  {:lang "en"}
+  [:head
+      (include-css "css/milligram.css")]
   [:body
-    [:div [:h1 "Hier sind die Daten:" ]]
-    [:div {:data-offset-top "250"}
+    [:div.container [:h1 "Übersicht Anmeldungen:" ]]
+    [:div.container {:data-offset-top "250"}
       [:table
+        [:thead
           [:tr
             [:td "Entfernen?"]
             [:td [:b "Vorname"]]
             [:td [:b "Nachname"]]
             [:td [:b "Anmeldedatum"]]
-            [:td [:b "Kommentar"]]]
+            [:td [:b "Kommentar"]]]]
+          [:tbody
           (for [{:keys [_id, firstname, lastname, timestamp, comment]} users]
             [:tr
-              [:td (form-to [:delete "./list"] (hidden-field :id _id) [:button.btn.btn-mini.btn-link {:type "submit"} [:i.icon-remove ]] (anti-forgery-field))]
+              [:td (form-to [:delete "./list"] (hidden-field :id _id) [:button.button-clear {:type "reset"} "entfernen"] (anti-forgery-field))]
               [:td (escape-html firstname)]
               [:td (escape-html lastname)]
               [:td (if timestamp
                      (local-date-time timestamp))]
               [:td (escape-html comment)]]
-          [:div  #"\n" "<br>"])]]]))
+          [:div  #"\n" "<br>"])]]]]))
