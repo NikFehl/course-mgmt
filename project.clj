f4d5d50427e1430756ac07589099fee2ea72b97d
NikFehl <niklasfehl@t-online.de>
2019-12-30 20:48:10 +0100

    added logger & upgraded other libraries to newest version

--- project.clj
+++ project.clj
@@ -2,12 +2,13 @@
   :description "For managing courses. functions: registration, waitlist, auto-reply"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
                  [ring "1.8.0"]
                  [ring/ring-defaults "0.3.2"]
-                 [com.novemberain/monger "3.1.0"]
-                 [clojure.java-time "0.3.2"]]
+                 [com.novemberain/monger "3.5.0"]
+                 [clojure.java-time "0.3.2"]
+                 [org.clojure/tools.logging "0.5.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler course-mgmt.handler/app}
   :main course-mgmt.core
