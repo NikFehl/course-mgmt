828f74e108e059378bce73665ca17ebad1ac7efa
NikFehl <niklasfehl@t-online.de>
2020-01-08 20:32:13 +0100

    playing with unit-tests.

--- test/course_mgmt/handler_test.clj
+++ test/course_mgmt/handler_test.clj
@@ -3,12 +3,29 @@
             [ring.mock.request :as mock]
             [course-mgmt.handler :refer :all]))
 
-(deftest test-app
+(deftest testing-gets
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:status response) 200))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))))
+
+  (testing "register-form"
+    (let [response (app (mock/request :get "/attendees/register"))]
+      (is (= (:status response) 200))))
+
+  (testing "list attendees"
+    (let [response (app (mock/request :get "/attendees/list"))]
+      (is (= (:status response) 200))))
+
+  (testing "list courses"
+    (let [response (app (mock/request :get "/courses/manage"))]
+      (is (= (:status response) 200)))))
+
+(deftest testing-posts
+  (testing "filter attendees"
+    (let [response (app (mock/request :post "/attendees/list" {:course "Hundekuchenessen" :sort-for "Alter"}))]
+      ;; response is forbidden due to anti-forgery-field.
+      (is (= (:status response) 403)))))
